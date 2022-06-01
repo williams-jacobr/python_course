f43aea61bf40d4df9d303def089c3a8b1cc8d49c
williams-jacobr <williams.jacobr@gmail.com>
2022-06-01 17:18:54 +0100

    Learning the very basics of Python

---
+++ data_types_operations.py
@@ -0,0 +1,37 @@
+#LISTS
+
+# monday_temperatures = ['hello',9.1, 8.8, 7.5]
+
+# monday_temperatures.append(8.1)
+
+# print(monday_temperatures)
+
+# print(monday_temperatures.index(8.8))
+
+# print(monday_temperatures.__getitem__(1))
+# print(monday_temperatures[1])
+# print(monday_temperatures[1:4])
+# print(monday_temperatures[0:2])
+# print(monday_temperatures[:2])
+# print(monday_temperatures[2:])
+# print(monday_temperatures[-1])
+# print(monday_temperatures[-2:])
+
+# print(monday_temperatures[0][2])
+
+
+
+# monday_temperatures.clear()
+
+# print(monday_temperatures)
+
+# mystring = 'hello'
+# print(mystring[1])
+# print(mystring[-1])
+# print(mystring[1:3])
+
+#DICTIONARIES
+
+student_grades = {'Mary': 9.1, 'Sim': 8.8, 'Joe': 7.5}
+
+print(student_grades['Mary'])
