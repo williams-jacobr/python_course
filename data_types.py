1b8232f150219ebe899d4a36de5919f61be97b5d
williams-jacobr <williams.jacobr@gmail.com>
2022-06-01 16:45:17 +0100

    Learning the very basics of Python

---
+++ data_types.py
@@ -0,0 +1,55 @@
+#INTEGERS STRINGS AND FLOATS
+
+# x = 10
+# y = '10'
+# z = 10.1
+
+# sum1 = x + x
+# sum2 = y + y
+
+# print(sum1, sum2)
+# print(type(x), type(y), type(z))
+
+
+#LISTS
+
+# student_grades = [9.1, 8.8, 7.5]
+
+# print(student_grades)
+
+# print(student_grades + student_grades)
+
+# print(student_grades * 2)
+
+#RANGES
+
+# student_grades = list(range(0,11))
+# student_grades2 = list(range(1, 9, 2))
+
+# print(student_grades)
+# print(student_grades2)
+
+#USING DIR
+
+# student_grades = [9.1, 8.8, 7.5]
+
+# mysum= sum(student_grades)
+# length = len(student_grades)
+
+# mean = mysum / length
+
+# print(mean)
+
+#DICTIONARIES
+
+# student_grades = {'Mary': 9.1, 'Sim': 8.8, 'Joe': 7.5}
+
+# mysum = sum(student_grades.values())
+# length=len(student_grades)
+# mean = mysum / length
+# print(mean)
+
+#TUPLES
+
+monday_temperatures = (1, 3, 6)
+print(monday_temperatures)
---
+++ my_programme.py
@@ -0,0 +1,21 @@
+# print (117 + 986)
+# print (199 - 981)
+
+# print(3 + 4 + 5 - 6)
+
+# print(3+4)
+# print (3 + 4)
+
+spent = 3
+donated = 4
+
+total_amount= spent + donated
+
+print(total_amount)
+
+items = 10
+price = 2
+
+total_price = items * price
+
+print(total_price)
