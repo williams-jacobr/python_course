267d2e99fce45d6204bf2c1b1572df67c4d74104
Jacob Williams <jacob.williams@psycle.com>
2022-08-10 11:31:57 +0100

    working with errors

---
+++ Error handling/errors.py
@@ -0,0 +1,15 @@
+# Lecture 1 - syntax
+
+# print(1)
+# int(9)
+# int(999)
+# print(2)
+# print 3
+
+# Lecture 2 - exceptions
+
+# a = 1
+# b = "2"
+# print(int(2.5))
+# print(a+b) - TypeError
+# print(c) - NameError
