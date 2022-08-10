61bd4f4251498ebbaf4bcdf3036b72c11a3bccd1
williams-jacobr <jacob.williams@psycle.com>
2022-08-10 14:28:24 +0100

    try except blocks

--- Error handling/errors.py
+++ Error handling/errors.py
@@ -13,3 +13,12 @@
 # print(int(2.5))
 # print(a+b) - TypeError
 # print(c) - NameError
+
+def divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return 'You are dividing by zero'
+
+print(divide(1,2))
+print(divide(1,0))
